e114d0ef667fc57204856ed920413ef387a5aa82
xiumeitang <xiumeitang@airasia.com>
2022-04-05 11:26:03 +0800

    feat: init project

--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@ const Koa = require('koa');
 const app = new Koa();
 const ipAddress = require('ip').address()
 const port = 5000
-// const host = '0.0.0.0'
-// const host = 'localhost'
 const chalk = require('chalk')
 
 app.use(ctx => {
@@ -11,11 +9,10 @@ app.use(ctx => {
 });
 
 app.listen(port, () => {
-  console.info('...npm_package_name:' + process.env.npm_package_name);
-  console.info('...npm_lifecycle_event:', process.env.npm_lifecycle_event);
-  console.info('\n***************************')
-  console.info('server start from at')
-  console.info(`http://localhost:${port}`)
-  console.info(`http://${ipAddress}:${port}`)
-  console.info('***************************\n')
+  // console.info(chalk.blue('...npm_package_name:' + process.env.npm_package_name));
+  // console.info(chalk.blue('...npm_lifecycle_event:', process.env.npm_lifecycle_event));
+  console.info(chalk.yellow('\n**************server start from at*************'))
+  console.info(chalk.green(`http://localhost:${port}`))
+  console.info(chalk.green(`http://${ipAddress}:${port}`))
+  console.info(chalk.yellow('***************************\n'))
 });
